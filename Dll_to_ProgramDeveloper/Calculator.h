cf422ec6ebb26c4a27d3d82c5693ec4e091108ce
ShrutiTiwarkar <Shrutiwarkar2196@gmail.com>
2019-08-31 11:08:15 -0700

    Done

---
+++ Dll_to_ProgramDeveloper/Calculator.h
@@ -0,0 +1,31 @@
+#pragma once
+// Addition of two Numbers.
+// Parameters : Two Integer.
+extern "C" int Addition(int, int);
+
+// Substraction of two Numbers.
+// Parameters : Two Integer.
+extern "C" int Substraction(int, int);
+
+// Multiplication of two Numbers.
+// Parameters : Two Integer.
+extern "C" int Multiplication(int, int);
+
+// Division of two Numbers.
+// Parameters : Two Integer.
+extern "C" int Division(int, int);
+
+// Modulus of two Numbers.
+// Parameters : Two Integer.
+extern "C" int Modulus(int, int);
+
+// Swapping Two Numbers using Third Variable
+// Parameters : Address of Two integers.
+extern "C" void SwapUsingTemp(int *, int *);
+
+// Swapping Two Numbers without Third Variable
+// Parameters : Address of Two integers.
+extern "C" void SwapWithoutTemp(int *, int *);
+
+
+
---
+++ Dll_to_ProgramDeveloper/ReadMe.txt
@@ -0,0 +1,21 @@
+This Progarm is about the various Calculations functions
+- Addition
+- Substraction
+- Multiplication
+- Division
+- Modulus
+- Swapping using 3rd variable
+- Swapping without using 3rd variable
+
+This also include Calculator.h file which contain the prototype of functions which are used in DLL.
+
+example:
+
+num1 = 50
+num2 = 10
+
+Here Just need to pass Two numbers to the Function(Addition, Substarction, Multiplication, Division, Modulus).
+
+Here just need to pass the address of two variable to the Funation(Swaping)
+
+DLL will be linked Explicityly ...Lib is not provided.
